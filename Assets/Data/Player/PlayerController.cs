c48de1b16843133df2ebd354a321c91d6471fca8
OddvarLookus <lorenzo1viglietti@gmail.com>
2022-02-20 18:11:11 +0100

    volo bene

--- Assets/Data/Player/PlayerController.cs
+++ Assets/Data/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float playerMaxSpeed;
+    [SerializeField] float playerDashMaxSpeed;
     [SerializeField] float playerAcceleration;
     [SerializeField] float playerDeceleration;
     [SerializeField] CameraController camController;
@@ -39,23 +40,42 @@ public class PlayerController : MonoBehaviour
         {
             inputVec.x += 1f;
         }
+        bool dashPressed = Input.GetKey(KeyCode.LeftShift);
+
 
         inputVec = inputVec.normalized;
         Vector3 relativeInput = Quaternion.LookRotation(camController.GetCameraForward(), camController.GetCameraUp()) * inputVec;
-        relativeInput = Vector3.ProjectOnPlane(relativeInput, Vector3.up).normalized;
-
-        rb.AddForce(relativeInput * playerAcceleration, ForceMode.Acceleration);
-        Vector3 hVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        if (hVel.magnitude > playerMaxSpeed)
-        {
-            hVel = Vector3.ClampMagnitude(hVel, playerMaxSpeed);
-            rb.velocity = new Vector3(hVel.x, rb.velocity.y, hVel.z);
-        }
+        relativeInput = relativeInput.normalized;
+        //relativeInput = Vector3.ProjectOnPlane(relativeInput, Vector3.up).normalized;
 
         if (inputVec != Vector3.zero)
         {
-
+            if (!dashPressed)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, relativeInput * playerMaxSpeed, playerAcceleration * Time.deltaTime);
+            }
+            else if (dashPressed)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, relativeInput * playerDashMaxSpeed, playerAcceleration * Time.deltaTime);
+            }
         }
+        else
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, relativeInput * playerMaxSpeed, playerDeceleration * Time.deltaTime);
+        }
+        
+
+        //rb.AddForce(relativeInput * playerAcceleration, ForceMode.Acceleration);
+        //Vector3 hVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //if (hVel.magnitude > playerMaxSpeed)
+        //{
+        //    hVel = Vector3.ClampMagnitude(hVel, playerMaxSpeed);
+        //    rb.velocity = new Vector3(hVel.x, rb.velocity.y, hVel.z);
+        //}
+        //if (inputVec == Vector3.zero)
+        //{
+        //    rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, playerDeceleration * Time.deltaTime);
+        //}
     }
 
 }
