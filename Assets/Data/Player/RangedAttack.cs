906646a4983435079a390393d2729278f6efc77c
OddvarLookus <lorenzo1viglietti@gmail.com>
2022-04-12 01:45:58 +0200

    shoot cooldown

--- Assets/Data/Player/RangedAttack.cs
+++ Assets/Data/Player/RangedAttack.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
@@ -6,9 +6,17 @@ using Sirenix.OdinInspector;
 public class RangedAttack : MonoBehaviour
 {
     [SerializeField] float maxRange;
-    [MinValue(0)][SerializeField] int maxTargetsNum;
+	[MinValue(0)][SerializeField] int maxTargetsNum;
+	[MinValue(0f)] [SerializeField] float cooldown;
+	float currentCooldown;
 
     [SerializeField] GameObject rangedProjectilePrefab;
+
+	
+	protected void OnEnable()
+	{
+		currentCooldown = cooldown;
+	}
 
     void Start()
     {
@@ -22,26 +30,36 @@ public class RangedAttack : MonoBehaviour
     }
 
     void AttackInput()
-    {
-        if(Input.GetMouseButtonDown(1))
-        {
-            Collider[] enemies = Physics.OverlapSphere(transform.position, maxRange, 1<<6, QueryTriggerInteraction.Ignore);
-            if(enemies.Length <= maxTargetsNum)//enemies in range are equal or less to the max number of targets
-            {
-                for(int i = 0; i < enemies.Length; i++)
-                {
-                    ShootProjectile(enemies[i].transform);
-                }
-            }
-            else//enemies in range are more than max number of targets
-            {
-                for(int i = 0; i < maxTargetsNum; i++)
-                {
-                    ShootProjectile(enemies[i].transform);
-                }
+	{
+		if(currentCooldown >= cooldown)
+		{
+			if(Input.GetMouseButtonDown(1))
+			{
+				Collider[] enemies = Physics.OverlapSphere(transform.position, maxRange, 1<<6, QueryTriggerInteraction.Ignore);
+				if(enemies.Length <= maxTargetsNum && enemies.Length > 0)//enemies in range are equal or less to the max number of targets
+				{
+					currentCooldown = 0f;
+					for(int i = 0; i < enemies.Length; i++)
+					{
+						ShootProjectile(enemies[i].transform);
+					}
+				}
+				else if(enemies.Length > maxTargetsNum && enemies.Length > 0)//enemies in range are more than max number of targets
+				{
+					currentCooldown = 0f;
+					for(int i = 0; i < maxTargetsNum; i++)
+					{
+						ShootProjectile(enemies[i].transform);
+					}
 
-            }
-        }
+				}
+			}
+		}
+		else
+		{
+			currentCooldown += Time.deltaTime;
+		}
+
     }
 
 
