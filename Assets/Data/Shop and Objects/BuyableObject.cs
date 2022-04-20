5d4eac6df6365e4b2fe1335c290a4d8d3dd7eaff
OddvarLookus <lorenzo1viglietti@gmail.com>
2022-04-21 01:30:19 +0200

    buyable object start

---
+++ Assets/Data/Shop and Objects/BuyableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyableObject : MonoBehaviour
+{
+	BuyableObjectInfos objectInfos;
+	public void SetObjectInfos(BuyableObjectInfos newObjectInfos)
+	{
+		objectInfos = newObjectInfos;
+		//InitializeGraphics();
+	}
+    
+	protected void OnEnable()
+	{
+		InitializeGraphics();
+	}
+    
+	void InitializeGraphics()
+	{
+		if(transform.childCount == 0)
+		{
+			GameObject gg = Instantiate(objectInfos.graphicsPrefab);
+			gg.transform.SetParent(transform);
+			gg.transform.localPosition = new Vector3(0f, 0f, 0f);
+		}
+	}
+    
+    
+}
+
+[System.Serializable]
+public class BuyableObjectInfos
+{
+	public float damageBonus;
+	public float attackSpeedBonus;
+	public int projectilesNumBonus;
+	
+	public GameObject graphicsPrefab;
+}
