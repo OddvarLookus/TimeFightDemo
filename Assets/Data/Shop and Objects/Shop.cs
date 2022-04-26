afba3154f3fe5f6f5592a4b8ee3f87c5074fc607
OddvarLookus <lorenzo1viglietti@gmail.com>
2022-04-26 22:31:46 +0200

    vfx and better shop

--- Assets/Data/Shop and Objects/Shop.cs
+++ Assets/Data/Shop and Objects/Shop.cs
@@ -13,24 +13,47 @@ public class Shop : MonoBehaviour, IInteractable
 	
 	[Header("Object Positions")]
 	[SerializeField] Transform pos0, pos1, pos2;
+	Vector3 localPos0, localPos1, localPos2;
 	
 	bool hasInteracted = false;
 	public void Interact(PlayerController pc)
 	{
 		if(!hasInteracted)
 		{
-			ThrowObjects();
+			bool fromFront = Vector3.Dot((pc.transform.position - transform.position).normalized, transform.forward) >= 0f;
+			
+			ThrowObjects(fromFront);
 			hasInteracted = true;
 		}
 		
 	}
 	
-	
-	void ThrowObjects()
+	protected void Start()
 	{
+		localPos0 = pos0.localPosition;
+		localPos1 = pos1.localPosition;
+		localPos2 = pos2.localPosition;
+	}
+	
+	void ThrowObjects(bool front)
+	{
+		if(front)
+		{
+			pos0.localPosition = localPos0;
+			pos1.localPosition = localPos1;
+			pos2.localPosition = localPos2;
+		}
+		else
+		{
+			pos0.localPosition = new Vector3(localPos0.x, localPos0.y, -localPos0.z);
+			pos1.localPosition = new Vector3(localPos1.x, localPos1.y, -localPos1.z);
+			pos2.localPosition = new Vector3(localPos2.x, localPos2.y, -localPos2.z);
+		}
+		
 		InstantiateBuyableObjectAtPos(pos0.position);
 		InstantiateBuyableObjectAtPos(pos1.position);
 		InstantiateBuyableObjectAtPos(pos2.position);
+
 	}
 	
 	void InstantiateBuyableObjectAtPos(Vector3 pos)
