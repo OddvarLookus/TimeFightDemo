d5f175be1edaf9459db883aaf3953c7e9e5fd423
OddvarLookus <lorenzo1viglietti@gmail.com>
2022-05-09 01:21:15 +0100

    skibox rotation

---
+++ Assets/Data/Physics Utilities/SkyboxRotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    
+	[SerializeField] float rotationSpeed;
+	float currentSkyboxRot = 0f;
+    
+	
+    
+	protected void Start()
+	{
+		
+	}
+    
+    void Update()
+	{
+		currentSkyboxRot += rotationSpeed * Time.deltaTime;
+		if(currentSkyboxRot > 360f)
+		{
+			currentSkyboxRot = currentSkyboxRot - 360f;
+		}
+		else if(currentSkyboxRot < 0f)
+		{
+			currentSkyboxRot = 360f + currentSkyboxRot;
+		}
+		RenderSettings.skybox.SetFloat("_Rotation", currentSkyboxRot);
+    }
+}
